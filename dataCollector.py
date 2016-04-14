ee8b3b85786978deb70668fe91580c0f35b4c4e9
Li Yi <jayceeyili@gmail.com>
2016-04-14 00:09:01 -0700

    add excel write process

--- dataCollector.py
+++ dataCollector.py
@@ -1,9 +1,11 @@
 import xlrd
+import xlwt
 from os import listdir
 
-cal_path = "/Users/yili/Desktop/test/calcheck/"
-bss_path = "/Users/yili/Desktop/test/bss/"
+cal_path = "/Users/yili/Desktop/HW/calcheck/"
+bss_path = "/Users/yili/Desktop/HW/bss/"
 
+sn_matrix = []
 matrix = []
 for files in listdir(cal_path):
     book_path = cal_path + files
@@ -12,6 +14,8 @@ for files in listdir(cal_path):
     t0 = book.sheet_by_name("-5.0C").cell_value(0, 1)
     t65 = book.sheet_by_name("65.0C").cell_value(0, 1)
     win_cal = book.sheet_by_name("Sum").row_values(8, 6, 9)
+    sn = eval(files[4: 12])
+    sn_matrix.append([sn, " "])
     matrix.append([t0, t38, t65])
     matrix.append(win_cal)
 
@@ -19,11 +23,23 @@ bss_matrix = []
 for files in listdir(bss_path):
     bookbss_path = bss_path + files
     bookbss = xlrd.open_workbook(bookbss_path, on_demand=True)
+    sn = bookbss.sheet_by_name("25C").cell_value(0, 1)
     t25 = bookbss.sheet_by_name("25C").cell_value(0, 2)
     win_bss = bookbss.sheet_by_name("Win Data").cell_value(0, 12)
     bss_matrix.append([t25, win_bss])
 
-for row in bss_matrix:
-    for value in row:
-        print(value, end=" ")
-    print()
+book = xlwt.Workbook()
+sheet1 = book.add_sheet('result', cell_overwrite_ok=True)
+
+l = [item for sublist in bss_matrix for item in sublist]
+s = [item for sublist in sn_matrix for item in sublist]
+
+for i, row in enumerate(matrix):
+    row.insert(0, l[i])
+    row.insert(0, s[i])
+
+for i, row in enumerate(matrix):
+    for j, col in enumerate(row):
+        sheet1.write(i, j, col)
+
+book.save("/Users/yili/Desktop/result.xls")

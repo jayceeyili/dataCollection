385feb2e77c9383aebef72973b65abf1f3b25958
Li Yi <jayceeyili@gmail.com>
2016-04-13 23:02:20 -0700

    add bss data collection

--- dataCollector.py
+++ dataCollector.py
@@ -1,11 +1,12 @@
 import xlrd
 from os import listdir
 
-path = "/Users/yili/Desktop/test/"
+cal_path = "/Users/yili/Desktop/test/calcheck/"
+bss_path = "/Users/yili/Desktop/test/bss/"
 
 matrix = []
-for files in listdir(path):
-    book_path = path + files
+for files in listdir(cal_path):
+    book_path = cal_path + files
     book = xlrd.open_workbook(book_path, on_demand=True)
     t38 = book.sheet_by_name("38.0C").cell_value(0, 1)
     t0 = book.sheet_by_name("-5.0C").cell_value(0, 1)
@@ -14,7 +15,15 @@ for files in listdir(path):
     matrix.append([t0, t38, t65])
     matrix.append(win_cal)
 
-for row in matrix:
+bss_matrix = []
+for files in listdir(bss_path):
+    bookbss_path = bss_path + files
+    bookbss = xlrd.open_workbook(bookbss_path, on_demand=True)
+    t25 = bookbss.sheet_by_name("25C").cell_value(0, 2)
+    win_bss = bookbss.sheet_by_name("Win Data").cell_value(0, 12)
+    bss_matrix.append([t25, win_bss])
+
+for row in bss_matrix:
     for value in row:
         print(value, end=" ")
     print()
